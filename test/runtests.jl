317d08b1eff7f5ff53489ff2e3fc6efb27d4d6d5
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-01-02 21:27:55 +0100

    Moved examples to test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,39 @@
 using Test
+using SightSeeing
 
-include("../src/SightSeeing.jl")
-using Main.SightSeeing
+ex_01 = quote
+    @new_slide
+    @explain "This is a generic explanation text."
+    @item_list "a" "b" "c"
+    @ask_continue
+end
 
-println("TEST 1")
-@explain "halo I bims"
+ex_02 = quote
+    @new_slide
+    @explain "This is an example of a REPL code demo."
+    @code_repl :(1 + 1)
+    @ask_continue
+end
 
-println("TEST 2")
-@explain "halo" "I" "bims"
+# ex_03 = quote
+#     @new_slide
+#     @code_snippet begin
+#         a = "halo "
+#         b = "i bims"
+#         print(a * b)
+#     end
+# end
 
+slide_01 = Slide(ex_01)
+slide_02 = Slide(ex_02)
+# slide_03 = Slide(ex_03)
+deck = SlideDeck([
+    slide_01,
+    slide_02,
+    # slide_03
+])
 
-println("TEST 3")
-@code_repl "for i in 1:5 print(i*5, \"\\n\") end"
+@test slide_01.content == ex_01
+@test deck.slides[1] == slide_01
 
-
-println("TEST 4")
-@code_repl for i in 1:5 print(i*5, "\n") end
-
-
+play(deck)
